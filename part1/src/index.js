400c00c5087921340f09eceaab238d766457e9f5
Tyler Fretz <fretztyler@gmail.com>
2020-11-23 17:17:52 -0500

    1.11

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,20 +14,68 @@ const Button = ({ handleClick, text}) => (
   </button>
 )
 
-const Display = ({ good, neutral, bad}) => (
-  <div>
-    <p>
-      good {good}
-    </p>
-    <p>
-      neutral {neutral}
-    </p>
-    <p>
-      bad {bad}
-    </p>
-  </div>
-)
+const Statistics = ({ stats }) => {
 
+  const getTotal = () => stats[0] + stats[1] + stats[2]
+
+  const getAverage = () => {
+    let totalResponse = getTotal()
+    let score = stats[0] - stats[2]
+    return (score / totalResponse).toFixed(2)
+  }
+
+  const getPositive = () => {
+    let totalResponse = getTotal()
+    return ((stats[0] / totalResponse) * 100).toFixed(2)
+  }
+
+  if ( getTotal() === 0 ) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  
+  return (
+    <table>
+      <thead>
+        <th>
+          Rating
+        </th>
+        <th>
+          Amount
+        </th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{stats[0]}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{stats[1]}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{stats[2]}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{getTotal() || 0}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{getAverage() || 0}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{getPositive() || 0} %</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -35,26 +83,15 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const setToGood = newGood => {
-    setGood(newGood)
-  }
-
-  const setToNeutral = newNeutral => {
-    setNeutral(newNeutral)
-  }
-
-  const setToBad = newBad => {
-    setBad(newBad)
-  }
 
   return (
     <div>
       <Title text="give feedback" />
-      <Button handleClick={() => setToGood(good + 1)} text="good" />
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Title text="statistics" />
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Statistics stats={[good, neutral, bad]} />
     </div>
   )
 }
