54c8c068e472c78997f3cfb80d2819f6d8f1b04b
Tyler Fretz <fretztyler@gmail.com>
2021-01-01 15:21:17 -0500

    add backend testing

--- part4/backend/app.js
+++ part4/backend/app.js
@@ -23,7 +23,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blog-app', blogsRouter)
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
