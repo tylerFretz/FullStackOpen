0020c5973ff2ba430167dd9ce3f34f9277b93946
Tyler Fretz <fretztyler@gmail.com>
2021-02-05 20:03:23 -0500

    9.19

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Diagnosis {
     code: string;
@@ -24,8 +26,58 @@ export enum Gender {
     Other = 'other'
 }
 
+export enum EntryType {
+    HealthCheck = "HealthCheck",
+    OccupationalHealthCare = "OccupationalHealthcare",
+    Hospital = "Hospital"
+}
+
+interface BaseEntry {
+    id: string;
+    type: EntryType;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+export interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: EntryType.HealthCheck;
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: EntryType.OccupationalHealthCare;
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: EntryType.Hospital;
+    discharge: Discharge;
+}
+
 export type NonSensitivePatient = Omit<Patient, 'ssn' >;
 
-export type NewPatient = Omit<Patient, 'id' >;
+export type NewPatient = Omit<Patient, 'id' | 'entries' >;
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
+
+export type NewBaseEntry = Omit<BaseEntry, 'id' >;
--- part9/patientor/src/utils.ts
+++ part9/patientor/src/utils.ts
@@ -1,11 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Gender, NewPatient } from './types';
+import { Gender, NewPatient, EntryType, NewBaseEntry } from './types';
 
 
-const isString = (text: any): text is string => {
+const isString = (text: any): boolean => {
     return typeof text === 'string' || text instanceof String;
 };
 
@@ -21,6 +23,21 @@ const isGender = (param: any): param is Gender => {
     return Object.values(Gender).includes(param);
 };
 
+const isEntryType = (param: any): param is EntryType => {
+    return Object.values(EntryType).includes(param);
+};
+
+const isArrayOfStrings = (param: any[]): boolean => {
+    const hasNonString: boolean = param.some((item: any) => {
+        return !isString(item);
+    });
+    return !hasNonString;
+};
+
+// const isHealthCheckRating = (param: any): boolean => {
+//     return Object.values(HealthCheckRating).includes(param);
+// };
+
 const parseName = (name: any): string => {
     if (!name || !isString(name)) {
         throw new Error('Incorrect or missing name: ' + name);
@@ -28,7 +45,7 @@ const parseName = (name: any): string => {
     return name;
 };
 
-const parseDateOfBirth = (date: any): string => {
+const parseDate = (date: any): string => {
     if (!date || !isString(date) || !isDate(date)) {
         throw new Error('Incorrect or missing date: ' + date);
     }
@@ -56,22 +73,94 @@ const parseOccupation = (occupation: any): string => {
     return occupation;
 };
 
+const parseEntryType = (type: any): EntryType => {
+    if (!type || !isString(type) || !isEntryType(type)) {
+        throw new Error('Incorrect or missing Entry Type: ' + type);
+    }
+    return type;
+};
+
+const parseDiagnosesCodes = (codes: any[]): string[] => {
+    if (!Array.isArray(codes) || !isArrayOfStrings(codes)) {
+        throw new Error("Incorrect or missing diagnoses");
+    }
+    return codes;
+};
+
+const parseDescription = (description: any): string => {
+    if (!description || !isString(description)) {
+        throw new Error("Incorrect or missing description: " + description);
+    }
+    return description;
+};
+
+const parseSpecialist = (specialist: any): string => {
+    if (!specialist || !isString(specialist)) {
+        throw new Error("Incorrect or missing specialist: " + specialist);
+    }
+    return specialist;
+};
+
+// const parseHealthCheckRating = (healthCheckRating: any): HealthCheckRating => {
+//     if (!healthCheckRating || !isHealthCheckRating(healthCheckRating)) {
+//         throw new Error("Incorrect or missing Health Check Rating: " + healthCheckRating);
+//     }
+//     return healthCheckRating;
+// };
+
+// const parseCriteria = (criteria: any): string => {
+//     if (!criteria || !isString(criteria)) {
+//         throw new Error("Incorrect or missing criteria: " + criteria);
+//     }
+//     return criteria;
+// };
+
+// const parseSickLeave = (object: any) => {
+//     if (!object)
+//         throw new Error("Missing sick leave");
+//     return {
+//         startDate: parseDate(object.startDate),
+//         endDate: parseDate(object.endDate),
+//     };
+// };
+
+// const parseDischarge = (object: any) => {
+//     if (!object)
+//         throw new Error("Missing discharge");
+//     return {
+//         date: parseDate(object.date),
+//         criteria: parseCriteria(object.criteria)
+//     };
+// };
+
 export const toNewPatient = (object: any): NewPatient => {
     return {
         name: parseName(object.name),
-        dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+        dateOfBirth: parseDate(object.dateOfBirth),
         ssn: parseSSN(object.ssn),
         gender: parseGender(object.gender),
-        occupation: parseOccupation(object.occupation),
-        entries: []
+        occupation: parseOccupation(object.occupation)
     };
+};
+
+export const toNewBaseEntry = (object: any): NewBaseEntry => {
+    const newBaseEntry: NewBaseEntry = {
+        type: parseEntryType(object.type),
+        description: parseDescription(object.description),
+        date: parseDate(object.date),
+        specialist: parseSpecialist(object.specialist)
+    };
+    if (object.diagnosisCodes) {
+        newBaseEntry.diagnosisCodes = parseDiagnosesCodes(object.diagnosisCodes);
+    }
+    return newBaseEntry;
 };
 
 export const makeId = (length: number): string => {
     let result = '';
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * length));
+        result += chars.charAt(Math.floor(Math.random() * chars.length - 1));
     }
     return result;
 };
